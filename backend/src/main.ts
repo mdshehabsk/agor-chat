9f33a5813a9bf29c80fbc127a6cedb8036ba7e76
mdshehabsk <mdshehabsk@gmail.com>
2025-09-29 12:51:39 +0600

    configure cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,18 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: {
-      origin: 'http://localhost:3000',
-    },
-  });
 
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // Get ConfigService first
   const configService = app.get(ConfigService);
+
+  // Enable CORS with correct options
+  app.enableCors({
+    origin: configService.get<string>('FRONTEND_URL'),
+    credentials: true,
+  });
 
   const port = configService.get<number>('PORT', 3000);
   await app.listen(port);
 
-  console.log(`server is running on port http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 }
+
 void bootstrap();
